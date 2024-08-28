54177e6f0c361542a306467d30846c5fc9542ac1
plskyuwu <gottsteinmatej@gmail.com>
2024-08-28 10:42:49 +0200

    Added libappindicator crate and updated all crates

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
 use std::{fs, thread, time};
 use notify_rust::Notification;
 
-fn main() {
-    let modes: [&str; 3] = ["Balanced", "Boost", "Silent"];
-    let mut previous_mode: usize = get_mode();
 
-    loop {
-        let fan_mode: usize = get_mode();
+fn main() 
+{
+  let modes: [&str; 3] = ["Balanced", "Boost", "Silent"];
+  let mut previous_mode: usize = get_mode();
 
-        if fan_mode != previous_mode {
-            println!("Current mode: {}", modes[fan_mode]);
-            Notification::new()
-                .summary(format!("Current fan mode: {}", modes[fan_mode]).as_str())
-                .show()
-                .unwrap();
-        }
-        previous_mode = fan_mode;
+  loop 
+  {
+    let fan_mode: usize = get_mode();
 
-        thread::sleep(time::Duration::from_secs(1));
+    if fan_mode != previous_mode 
+    {
+      println!("Current mode: {}", modes[fan_mode]);
+      Notification::new()
+        .summary(format!("Current fan mode: {}", modes[fan_mode]).as_str())
+        .show()
+        .unwrap();
     }
+    previous_mode = fan_mode;
+
+    thread::sleep(time::Duration::from_secs(1));
+  }
 }
 
-fn get_mode() -> usize {
-    let file_path: &str = "/sys/devices/platform/asus-nb-wmi/fan_boost_mode";
 
-    return fs::read_to_string(file_path)
-        .expect("Should have been able to read the file")
-        .trim()
-        .to_string()
-        .parse::<usize>()
-        .unwrap();
+fn get_mode() -> usize 
+{
+  let file_path: &str = "/sys/devices/platform/asus-nb-wmi/fan_boost_mode";
+
+  return fs::read_to_string(file_path)
+    .expect("Should have been able to read the file")
+    .trim()
+    .to_string()
+    .parse::<usize>()
+    .unwrap();
 }
