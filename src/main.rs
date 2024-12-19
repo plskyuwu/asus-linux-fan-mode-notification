a3e9f386ba9be9f339026b11bffd9c4de464216d
plskyuwu <gottsteinmatej@gmail.com>
2024-12-19 10:31:18 +0100

    edit: made code compliant with rust style guidelines

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
-use std::{fs, thread, time};
 use notify_rust::Notification;
+use std::{fs, thread, time};
 
+fn main() {
+    let modes: [&str; 3] = ["Balanced", "Boost", "Silent"];
+    let mut previous_mode: usize = get_mode();
 
-fn main() 
-{
-  let modes: [&str; 3] = ["Balanced", "Boost", "Silent"];
-  let mut previous_mode: usize = get_mode();
+    loop {
+        let fan_mode: usize = get_mode();
 
-  loop 
-  {
-    let fan_mode: usize = get_mode();
+        if fan_mode != previous_mode {
+            println!("Current mode: {}", modes[fan_mode]);
+            Notification::new()
+                .summary(format!("Current fan mode: {}", modes[fan_mode]).as_str())
+                .show()
+                .unwrap();
+        }
+        previous_mode = fan_mode;
 
-    if fan_mode != previous_mode 
-    {
-      println!("Current mode: {}", modes[fan_mode]);
-      Notification::new()
-        .summary(format!("Current fan mode: {}", modes[fan_mode]).as_str())
-        .show()
-        .unwrap();
+        thread::sleep(time::Duration::from_secs(1));
     }
-    previous_mode = fan_mode;
-
-    thread::sleep(time::Duration::from_secs(1));
-  }
 }
 
+fn get_mode() -> usize {
+    let file_path: &str = "/sys/devices/platform/asus-nb-wmi/fan_boost_mode";
 
-fn get_mode() -> usize 
-{
-  let file_path: &str = "/sys/devices/platform/asus-nb-wmi/fan_boost_mode";
-
-  return fs::read_to_string(file_path)
-    .expect("Should have been able to read the file")
-    .trim()
-    .to_string()
-    .parse::<usize>()
-    .unwrap();
+    return fs::read_to_string(file_path)
+        .expect("Should have been able to read the file")
+        .trim()
+        .to_string()
+        .parse::<usize>()
+        .unwrap();
 }
